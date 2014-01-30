150a49672916dd386511e305e039772d133ba302
James Mills <prologic@shortcircuit.net.au>
2014-01-30 22:33:07 +1000

    Use binary docker from upstream

--- post-build.sh
+++ post-build.sh
@@ -12,6 +12,5 @@ chmod 700 $TARGET/root/.ssh
 chmod 400 $TARGET/root/.ssh/*
 
 # Install docker
-#curl -q -o $TARGET/usr/bin/docker https://get.docker.io/builds/Linux/x86_64/docker-latest
-cp /home/prologic/docker/bundles/0.7.6/binary/docker-0.7.6 $TARGET/usr/bin/docker
+curl -q -o $TARGET/usr/bin/docker https://get.docker.io/builds/Linux/x86_64/docker-latest
 chmod 755 $TARGET/usr/bin/docker
